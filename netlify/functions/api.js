837ea219d428340a98bcea43130ec3a3e22981a2
williamgl3 <22620042@tlaxiaco.tecn.mx>
2026-05-29 13:23:55 -0600

    fix: add basePath /api to serverless-http for Netlify path routing

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,4 +2,4 @@ import serverless from 'serverless-http';
 import { createApp } from '../../server/index.js';
 
 const app = createApp();
-export const handler = serverless(app);
+export const handler = serverless(app, { basePath: '/api' });
